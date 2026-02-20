226a3363e0d678f03c959f15731e769a69ca4fda
JanagiramK <janagi2368@gmail.com>
2026-02-20 11:09:45 +0530

    feature: Add CI pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello Devops");
+console.log("Learning CI/CD using github actions");
+
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests...");
+SetTimeout(() => {
+    console.log("Waiting 3 seconds ...");
+}, 3000);
+log("Tests completed.");
